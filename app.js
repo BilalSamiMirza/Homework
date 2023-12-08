eccd70ddc3308bff10ffc14788231ac524c1ee91
BilalSamiMirza <bilalsamimirza@gmail.com>
2023-12-09 00:50:45 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,90 @@
+alert("hellojavascripht")
+alert("This page is responsive?")
+var firstPersonName = "zaid"
+console.log(firstPersonName)
+
+
+var marks = 75;
+alert(marks)
+
+var marks = 85;
+alert(marks)
+
+
+var a = 10;
+var b = a++;
+var b = a+1;
+console.log(b, "B value");
+console.log(a, "A value");
+var userName = prompt("Enter Your Name");
+var age = prompt("Enter Your Age");
+console.log(userName, age);
+//var age = "12";
+// if(age ==="20"){
+//     console.log("condition True");}
+
+//== , === EQUAL TO
+//var studentcard = false;
+//if(age >= 15) {
+//     console.log("Allow");
+//} else if (studentcard == true){
+//  console.log("allow on student card");
+//} else{
+//  console.log("Not Allow");}
+// var age = "13";
+// var cnic = true;
+// var studentcard = false;
+
+// if (age >=18 && cnic == true){
+//     console.log("Allow");
+// }
+// else {
+//     console.log("Not Allow");
+// }
+
+
+
+// if (age >=18 || studentcard == true){
+//     console.log("Allow");
+// }
+// else {
+//     console.log("Not Allow");
+// }
+// var a = "A";
+// var a = "B";
+
+// var arr = [ "A" , "B" , "C"];
+
+// console.log(arr);
+
+// arr[2] =12;
+
+// console.log(arr);
+
+// arr[5] ="0";
+// console.log(arr);
+
+// arr.push("D");
+// console.log(arr);
+
+var arr = [ "A" , "B" , "C" , "D" , "E"];
+console.log(arr);
+// arr.push("D");
+// arr.pop("D");
+
+// arr.shift("");
+// arr.unshift("2");
+
+// var a=arr.slice(1,4);
+
+arr.splice(2, 2, "c")
+
+console.log(a)
+
+
+
+
+
+
+
+
